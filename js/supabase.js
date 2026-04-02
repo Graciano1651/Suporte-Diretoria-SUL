3990908f9eea2069457a5e3664df04d42be17a04
graciano1651 <gui_graciano@hotmail.com>
2026-04-02 17:27:21 -0300

    introduzindo Supabase

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://trgsnbushnfdnixspnbl.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_0EA8y3PZBpzQOrzmYwWNPQ_M6P2n7QT'
+
+window.db = 
+window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
